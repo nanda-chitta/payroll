cbc331566e48e22836b21ed3c8e311a48d4f32e6
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2026-04-21 17:05:51 +0530

    Fix Render boot env handling for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,16 @@
-hosts = [
-  *ENV.fetch('DEV_DOMAIN').split(','),
-  *ENV.fetch('PROD_DOMAIN').split(',')
-].reject(&:blank?)
+hosts = %w[DEV_DOMAIN PROD_DOMAIN FRONTEND_URL]
+  .flat_map { |key| ENV.fetch(key, '').split(',') }
+  .map(&:strip)
+  .reject(&:blank?)
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins(*hosts)
+if hosts.any?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins(*hosts)
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post options delete put patch head]
+      resource '*',
+               headers: :any,
+               methods: %i[get post options delete put patch head]
+    end
   end
 end
